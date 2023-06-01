4f44ea152653a0758788d6d64503da5458881f67
Jeffrey Noel L. Quilaquil <jquilaquil@in.dbm.gov.ph>
2023-06-01 21:00:07 +0800

    first commit

---
+++ resources/views/library.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+@section('content')
+    <div class="row">
+      <div class="col-lg-8 col-md-6 mb-md-0 mb-4">
+        <div class="card">
+          <div class="card-header pb-0">
+            <div class="row">
+              <div class="col-lg-6 col-7"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+@endsection
